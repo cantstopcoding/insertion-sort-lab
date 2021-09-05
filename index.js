ef18fdc31e765382a36ad5c8440d6d07dd41bbea
cantstopcoding <jonathan_koss@yahoo.com>
2021-09-05 13:29:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-function findMinAndRemove(array) {
-  let min = array[0];
-  let minIndex = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < min) {
-      min = array[i];
-      minIndex = i;
+function findMinAndRemove(arr) {
+  let min = arr[0];
+  let lowestIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+      lowestIndex = i;
     }
   }
-  array.splice(minIndex, 1);
+  arr.splice(lowestIndex, 1);
   return min;
 }
 
-function selectionSort(array) {
-  let newMin;
-  let sorted = [];
-  while (array.length != 0) {
-    newMin = findMinAndRemove(array);
-    sorted.push(newMin);
+function selectionSort(arr) {
+  const arrangedArr = [];
+
+  while (!!arr.length) {
+    arrangedArr.push(findMinAndRemove(arr));
   }
-  return sorted;
+
+  return arrangedArr;
 }
